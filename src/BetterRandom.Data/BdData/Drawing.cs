2f35150f94737b8b94e0d52cffc894680fb7a352
LonelyDragon <illialubenets@yandex.ua>
2016-06-08 00:45:13 +0300

    Add jquery, bootstrap, Drawing model

---
+++ src/BetterRandom.Data/BdData/Drawing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BetterRandom.Data.BdData
+{
+    public class Drawing
+    {
+        public int Id { get; set; }
+        public int AuthorId { get; set; }
+        public int ParticipantsIds { get; set; }
+        public int WinnerIds { get; set; }
+        public int BlockIds { get; set; }
+    }
+}
